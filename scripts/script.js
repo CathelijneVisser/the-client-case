4de99274cc68021f2d32ea5a97e1c5fbd01ad639
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-10-26 10:24:33 +0200

    wip

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
 const allButtons = document.querySelectorAll(".js-expand");
 
 allButtons.forEach((button) => {
-  button.addEventListener("click", function () {
-    console.log(button);
-    button.parentElement.classList.toggle("expand");
-  });
+	button.addEventListener("click", function () {
+		console.log(button);
+		button.parentElement.classList.toggle("expand");
+	});
 });
+
+const headerTrigger = document.querySelector(".header__trigger");
+const headerList = document.querySelector(".header__list");
+
+if (headerTrigger) {
+	headerTrigger.addEventListener("click", () => {
+		headerTrigger.classList.toggle("is-open");
+		headerList.classList.toggle("is-open");
+	});
+}
